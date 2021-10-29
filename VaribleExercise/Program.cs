39023c17495a93d4d7b72fc56cab6085122e3a28
chelle07 <81632800+chelle07@users.noreply.github.com>
2021-10-29 14:40:32 -0700

    initial commit

---
+++ VaribleExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VaribleExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string catName = "Beth";
+            int catAge = 2;
+            char nickName = 'B';
+            bool isFunny = true;
+            double catToys = 12;
+            decimal catWeight = 17.4m;
+            {
+                Console.WriteLine($"My cats name is {catName} and she is {catAge} " +
+                    $"years old. Sometimes we call her by her nickname {nickName} and it's {isFunny} that " +
+                    $"she is funny. She has {catToys} cat toys that she plays with that keeps her at her weight of {catWeight} pounds.");
+            }
+
+
+        }
+    }
+}
